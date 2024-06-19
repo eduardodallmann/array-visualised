6a29fafe0e8e5df3374ed295652b2ddddaf386a1
Eduardo Dallmann <eduardodallmann@gmail.com>
2024-06-19 15:05:33 -0300

    reverse

--- src/app/components/mutator-arrays/reverse.tsx
+++ src/app/components/mutator-arrays/reverse.tsx
@@ -1,3 +1,30 @@
+import { ArrayBlock } from '../commons/array-block';
+import { ArrayFunctionBlockWrapper } from '../commons/array-function-block-wrapper';
+import { Arrow } from '../commons/arrow';
+import { ExampleWrapper } from '../commons/example-wrapper';
+import { FunctionBlock } from '../commons/function-block';
+import { Colors, type FunctionBlockType } from '../commons/types';
+
+const reverse1: Array<FunctionBlockType> = [
+  { id: crypto.randomUUID(), color: Colors.orange },
+  { id: crypto.randomUUID(), color: Colors.blue },
+  { id: crypto.randomUUID(), color: Colors.green },
+  { id: crypto.randomUUID(), color: Colors.purple },
+];
+const reverseResult1: Array<FunctionBlockType> = [...reverse1];
+reverseResult1.reverse();
+
 export function Reverse() {
-  return <>reverse wip</>;
+  return (
+    <>
+      <ExampleWrapper explanation="Reverse: Inverte a ordem dos elementos no array.">
+        <ArrayFunctionBlockWrapper>
+          <ArrayBlock itens={reverse1} showBreak />
+          <FunctionBlock functionName="reverse" itens={[]} />
+        </ArrayFunctionBlockWrapper>
+        <Arrow />
+        <ArrayBlock itens={reverseResult1} />
+      </ExampleWrapper>
+    </>
+  );
 }
